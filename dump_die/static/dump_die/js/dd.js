05eed12843190662d7d9ce4a2d4a9ab3bc3daa8b
David Barnes <barnesdavidj@gmail.com>
2022-03-17 14:05:17 -0400

    Update how the javascript is written to be clearer. Add more documentation to the JS.

--- dump_die/static/dump_die/js/dd.js
+++ dump_die/static/dump_die/js/dd.js
@@ -1,129 +1,165 @@
-
-$(document).ready(function() {
+const djangoDumpDie = {
 
     /**
+     * Set up the ability to have duplicated unique identifiers highlight on hover.
+     *
      * A "Unique" refers to an instance of a unique variable.
      * If two or more variables point to the same memory instance of a value/object,
-     * then they are grouped as a single "Unique".
+     * then they are listed as a single "Unique".
+     * TODO: Consider making the highlight scoped per dump call.
+     * Right now looks through for whole page, so if a var is dumped twice,
+     * or even appears twice via another dumped object it will highlight.
      */
-    // Add class to uniques that are duplicated for highlighting on hover.
-    // Var for all uniques.
-    var allUniques = [];
-    // Get all uniques from page.
-    const allUniquesJQ = $('.unique');
-    // Convert JQ uniques to array of unique text.
-    allUniquesJQ.each(function(index) {
-        allUniques.push($(this).html());
-    });
+    setUpUniqueDupHighlighting: () => {
+        // Var for all uniques.
+        let allUniques = [];
+        // Get all uniques from page via jQuery.
+        const allUniquesJQ = $('.unique');
+        // Convert JQ uniques to array of unique text.
+        allUniquesJQ.each(function(index) {
+            allUniques.push($(this).html());
+        });
 
-    // Function to find duplicates inside our dataset.
-    const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index);
+        // Function to find duplicates inside our dataset.
+        const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index);
 
-    // Find the duplicates and save to separate array.
-    const dupUniques = toFindDuplicates(allUniques);
+        // Find the duplicates and save to separate array.
+        const dupUniques = toFindDuplicates(allUniques);
 
-    // Iterate through each Unique found.
-    allUniquesJQ.each(function(index) {
+        // Iterate through all Uniques found.
+        allUniquesJQ.each(function(index) {
 
-        // Check if Unique has one or more instances.
-        // If so, set a class identifier on all matching elements which match said Unique.
-        if (dupUniques.includes($(this).html())) {
-            // Create the css class to identify Unique group.
-            var cssClass = 'duplicate-' + $(this).html();
+            // Check if Unique has one or more instances.
+            // If so, set a class identifier on all matching elements which match said Unique.
+            if (dupUniques.includes($(this).html())) {
 
-            // Add the css class to the element.
-            $(this).addClass(cssClass);
+                // Create the css class to identify Unique group.
+                var cssClass = 'duplicate-' + $(this).html();
 
-            // Append a period to the cssClass for jQuery.
-            cssClass = '.'+cssClass;
+                // Add the css class to the element.
+                $(this).addClass(cssClass);
 
-            // Get original foreground and background color.
-            var origForegroundColor = $(cssClass).css('color');
-            var origBackgroundColor = $('body').css('background-color');
+                // Append a period to the cssClass for jQuery.
+                cssClass = '.'+cssClass;
 
-            // On ALL elements with the class, on hover, swap the colors.
-            $(cssClass).hover(function() {
-                // Swap colors.
-                $(cssClass).css('color', origBackgroundColor);
-                $(cssClass).css('background-color', origForegroundColor);
-            }, function() {
-                // onMouseOut restore colors.
-                $(cssClass).css('color', "");
-                $(cssClass).css('background-color', "");
-            });
+                // Get original foreground and background color.
+                var origForegroundColor = $(cssClass).css('color');
+                var origBackgroundColor = $('body').css('background-color');
 
-        }
+                // On ALL elements with the class, on hover, swap the colors.
+                $(cssClass).hover(function() {
+                    // Swap colors.
+                    $(cssClass).css('color', origBackgroundColor);
+                    $(cssClass).css('background-color', origForegroundColor);
+                }, function() {
+                    // onMouseOut restore colors.
+                    $(cssClass).css('color', "");
+                    $(cssClass).css('background-color', "");
+                });
 
-    });
-
-
-    // Allow ability to CTRL click on an expandable element and have it auto
-    // expand all children elements that can be expanded.
-    $('.arrow-toggle').click(function(event) {
-
-        // Check if CTRL key was pressed.
-        if (event.ctrlKey) {
-            // Get the sibling ddwrapper for the clicked anchor tag.
-            var siblingDDWrapper = $(event.currentTarget).siblings('.dd-wrapper, .li-wrapper');
-
-            // If the ddwrapper has the class show, we want to remove the show class from all other
-            // ddwrappers as this one is about to loose that class from the bootstrap event.
-            // NOTE: Could be race condition on whether this is right order.
-            if ($(siblingDDWrapper).hasClass('show')) {
-                childDivs = $(siblingDDWrapper).find('.dd-wrapper, .li-wrapper');
-                childDivs.removeClass('show');
-
-                childArrows = $(siblingDDWrapper).find('.arrow');
-                childArrows.html('▶');
-            // Else, need to add show class to all elements.
-            } else {
-                childDivs = $(siblingDDWrapper).find('.dd-wrapper, .li-wrapper');
-                childDivs.addClass('show');
-
-                childArrows = $(siblingDDWrapper).find('.arrow');
-                childArrows.html('▼');
             }
-        }
 
-    });
+        });
+    },
 
 
-    // Update arrow on show.
-    $('.dd-wrapper').on('show.bs.collapse', function(event) {
-        unique = $(event.target).data('unique');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▼');
-    });
-    // Update arrow on hide.
-    $('.dd-wrapper').on('hide.bs.collapse', function(event) {
-        unique = $(event.target).data('unique');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▶');
-    });
+    /**
+     * Set up the ability to hold the CTRL key while clicking on an expandable
+     * element and have it auto expand all child elements that can also be
+     * expanded.
+     */
+    setUpCTRLClickFunctionality: () => {
+        // Set up click listener on an arrow-toggle
+        $('.arrow-toggle').click(function(event) {
 
-    // Update arrow on show attributes header.
-    $('.dd-wrapper').on('show.bs.collapse', function(event) {
-        unique = $(event.target).data('unique-attributes');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▼');
-    });
-    // Update arrow on hide.
-    $('.dd-wrapper').on('hide.bs.collapse', function(event) {
-        unique = $(event.target).data('unique-attributes');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▶');
-    });
+            // Check if CTRL key was pressed.
+            if (event.ctrlKey) {
+                // Get the sibling dd-wrapper or li-wrapper for the clicked anchor tag.
+                // This is the div that will be expanded via the click and bootstrap.
+                let siblingDDWrapper = $(event.currentTarget).siblings('.dd-wrapper, .li-wrapper');
 
-    // Update arrow on show functions header.
-    $('.dd-wrapper').on('show.bs.collapse', function(event) {
-        unique = $(event.target).data('unique-functions');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▼');
-    });
-    // Update arrow on hide.
-    $('.dd-wrapper').on('hide.bs.collapse', function(event) {
-        unique = $(event.target).data('unique-functions');
-        arrow_element = $('#arrow-' + unique);
-        $(arrow_element).html('▶');
-    });
+                // Get all dd-wrapper and li-wrapper child divs that need to be updated.
+                childDivs = $(siblingDDWrapper).find('.dd-wrapper, .li-wrapper');
+                // Get all arrows that should also be updated.
+                childArrows = $(siblingDDWrapper).find('.arrow');
+
+                // If the dd-wrapper or li-wrapper has the class show, we want
+                // to remove the show class from all other dd-wrappers as this
+                // one is about to loose that class from the bootstrap event.
+                // NOTE: Could be race condition on whether this is right order.
+                if ($(siblingDDWrapper).hasClass('show')) {
+                    // Remove the show class.
+                    childDivs.removeClass('show');
+                    // Change the arrow to the closed state.
+                    childArrows.html('▶');
+                // Else, need to add show class to all elements.
+                } else {
+                    // Add the show class.
+                    childDivs.addClass('show');
+                    // Change the arrow to the open state.
+                    childArrows.html('▼');
+                }
+            }
+
+        });
+    },
+
+    /**
+     * Hook into the events raised by Bootstrap when the collapse functionality
+     * is triggered to also update the arrow so that it is displayed correctly.
+     */
+    setUpArrowUpdating: () => {
+        // Update arrow on show.
+        $('.dd-wrapper').on('show.bs.collapse', function(event) {
+            unique = $(event.target).data('unique');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▼');
+        });
+        // Update arrow on hide.
+        $('.dd-wrapper').on('hide.bs.collapse', function(event) {
+            unique = $(event.target).data('unique');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▶');
+        });
+
+        // Update arrow on show attributes header.
+        $('.dd-wrapper').on('show.bs.collapse', function(event) {
+            unique = $(event.target).data('unique-attributes');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▼');
+        });
+        // Update arrow on hide attributes header.
+        $('.dd-wrapper').on('hide.bs.collapse', function(event) {
+            unique = $(event.target).data('unique-attributes');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▶');
+        });
+
+        // Update arrow on show functions header.
+        $('.dd-wrapper').on('show.bs.collapse', function(event) {
+            unique = $(event.target).data('unique-functions');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▼');
+        });
+        // Update arrow on hide functions header.
+        $('.dd-wrapper').on('hide.bs.collapse', function(event) {
+            unique = $(event.target).data('unique-functions');
+            arrow_element = $('#arrow-' + unique);
+            $(arrow_element).html('▶');
+        });
+    }
+}
+
+/**
+ * Code to be run when the document is fully loaded.
+ */
+$(document).ready(function() {
+
+    // Set up the unique duplicate highlighting
+    djangoDumpDie.setUpUniqueDupHighlighting();
+    // Set up the CTRL click functionality
+    djangoDumpDie.setUpCTRLClickFunctionality();
+    // Set up the Arrow Updating
+    djangoDumpDie.setUpArrowUpdating();
+
 });
