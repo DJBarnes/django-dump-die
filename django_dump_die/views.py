a3950bd3fa27482efb6dc9a14a520cef3196f9dc
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-03-25 08:18:29 -0400

    Document dd_view function

--- django_dump_die/views.py
+++ django_dump_die/views.py
@@ -5,25 +5,27 @@ from django.shortcuts import render
 
 
 def dd_view(request, objects):
-    """Return dump die view"""
-
+    """
+    Return dump die view.
+    :param request: Request object.
+    :param objects: Set of objects to dump.
+    """
+    # Get user theme choices.
     force_light_theme = getattr(settings, 'DJANGO_DD_FORCE_LIGHT_THEME', False)
     force_dark_theme = getattr(settings, 'DJANGO_DD_FORCE_DARK_THEME', False)
     custom_color_theme = getattr(settings, 'DJANGO_DD_COLOR_SCHEME', None)
 
+    # Validate chosen themes.
     if force_light_theme and force_dark_theme:
         raise ValueError("You can't force both light and dark themes.")
 
-    return render(
-        request,
-        'django_dump_die/dd.html',
-        {
-            'objects': objects,
-            'force_light_theme': force_light_theme,
-            'force_dark_theme': force_dark_theme,
-            'custom_color_theme': custom_color_theme,
-        }
-    )
+    # Render template.
+    return render(request, 'django_dump_die/dd.html', {
+        'objects': objects,
+        'force_light_theme': force_light_theme,
+        'force_dark_theme': force_dark_theme,
+        'custom_color_theme': custom_color_theme,
+    })
 
 
 def example(request):
