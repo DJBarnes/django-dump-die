efaeb3e27054dcf707978e64cf5f0ee496ff40f7
David Barnes <barnesdavidj@gmail.com>
2022-04-15 09:36:04 -0400

    Update the demo data to include a lot more scenarios.

--- django_dump_die/views.py
+++ django_dump_die/views.py
@@ -36,12 +36,86 @@ def dd_view(request, objects):
 def example(request):
     """Example Test"""
 
-    def test_func():
-        """Test doc string"""
+    SAMPLE_CONST = 'Sample Constant Content'
+    sample_module = ModuleType('django.html')
+    sample_bytes = b'sample bytes'
+    sample_date = datetime.now().date()
+    sample_datetime = datetime.now()
+    sample_time = datetime.now().time()
+    sample_int = 42
+    sample_float = 42.42
+    sample_decimal = Decimal(42.42)
+    sample_string = 'Sample String Content'
+    sample_none = None
+    sample_bool = True
+
+    sample_set = {'A', 'B', 'C',}
+    sample_tuple = ('A', 12, True)
+    sample_list = ['A', 12, True]
+    sample_dict = {
+        'first': 'A',
+        'second': 12,
+        'third': True,
+    }
+
+    sample_complex_set = {
+        (
+            'A',
+            12,
+            True,
+        ),
+        (
+            'B',
+            24,
+            False,
+        ),
+    }
+
+    sample_complex_tuple = (
+        {
+            'first': 'A',
+            'second': 12,
+            'third': True,
+        },
+        {
+            'fourth': 'B',
+            'fifth': 24,
+            'sixth': False,
+        },
+    )
+
+    sample_complex_list = [
+        {
+            'first': 'A',
+            'second': 12,
+            'third': True,
+        },
+        {
+            'fourth': 'B',
+            'fifth': 24,
+            'sixth': False,
+        },
+    ]
+
+    sample_complex_dict = {
+        'initial': {
+            'first': 'A',
+            'second': 12,
+            'third': True,
+        },
+        'secondary': {
+            'fourth': 'B',
+            'fifth': 24,
+            'sixth': False,
+        },
+    }
+
+    def sample_func():
+        """Sample doc string"""
         return 42
 
-    def test_func_param(param1, *args, **kwargs):
-        """Test param doc string"""
+    def sample_func_param(param1, *args, **kwargs):
+        """Sample param doc string"""
         return param1
 
     class EmptyClass:
@@ -49,79 +123,123 @@ def example(request):
         pass
 
 
-    class SpamClass:
+    class SimpleClass:
         """Spam sample class."""
+
+        SAMPLE_CLASS_CONST = 'Sample Class Constant Content'
+
+        sample_class_var = 23
+
         def __init__(self):
-            self._spam = 'eggs'
+            self._sample_private_module = ModuleType('django.html')
+            self._sample_private_bytes = b'sample bytes'
+            self._sample_private_date = datetime.now().date()
+            self._sample_private_datetime = datetime.now()
+            self._sample_private_time = datetime.now().time()
+            self._sample_private_int = 42
+            self._sample_private_float = 42.42
+            self._sample_private_decimal = Decimal(42.42)
+            self._sample_private_string = 'Sample String Content'
+            self._sample_private_none = None
+            self._sample_private_bool = True
+
+            self._sample_private_set = {'A', 'B', 'C',}
+            self._sample_private_tuple = ('A', 12, True)
+            self._sample_private_list = ['A', 12, True]
+            self._sample_private_dict = {
+                'first': 'A',
+                'second': 12,
+                'third': True,
+            }
+
+            self.sample_public_module = ModuleType('django.html')
+            self.sample_public_bytes = b'sample bytes'
+            self.sample_public_date = datetime.now().date()
+            self.sample_public_datetime = datetime.now()
+            self.sample_public_time = datetime.now().time()
+            self.sample_public_int = 42
+            self.sample_public_float = 42.42
+            self.sample_public_decimal = Decimal(42.42)
+            self.sample_public_string = 'Sample String Content'
+            self.sample_public_none = None
+            self.sample_public_bool = True
+
+            self.sample_public_set = {'A', 'B', 'C',}
+            self.sample_public_tuple = ('A', 12, True)
+            self.sample_public_list = ['A', 12, True]
+            self.sample_public_dict = {
+                'first': 'A',
+                'second': 12,
+                'third': True,
+            }
+
+        def sample_class_func(self):
+            """Sample class func doc string"""
+            return 'Sample class result'
+
+        def sample_class_param_func(self, arg1, *args, **kwargs):
+            """Sample class param func doc string"""
+            return arg1
 
 
-    class SomeClass:
-        """Sample class."""
-        def __init__(self, value):
-            self.viewable = 'viewable'
-            self._value = value
-            self._spam = SpamClass()
+    class ComplexClass:
+        """Complex class with nested instances"""
 
-        def get(self):
-            """Get class level value"""
-            return self._value
-
-        def add(self, x, y):
-            """Add two numbers"""
-            return x + y
-
-        def _secret(self):
-            """This is a private function"""
-            return 'secret'
+        def __init__(self):
+            self._sample_private_simple_class = SimpleClass()
+            self.sample_public_simple_class = SimpleClass()
+            self.duplicate_sample_public_simple_class = self.sample_public_simple_class
 
 
-    class SomeOtherClass:
-        """Some Other Class."""
-        SAMPLE_CONST = 41
-
-        def __init__(self, *args, **kwargs):
-            self.my_number = 32
-            self.my_float = 32.5
-            self.my_decimal = Decimal(32.5)
-            self.my_string = 'A super cool string'
-            self.works = True
-            self.nothing = None
-            self.bytes = bytes('My Bytes', 'utf-8')
-            self.datetime = datetime.now()
-            self.list_o_stuff = ['A', 'B', 'C']
-            self.sample_set = {'A', 'B', 'C'}
-            self.sample_tuple = ('A', 12, True)
-            self.some_class = SomeClass('sample')
-            self.some_class_dup = self.some_class
-
-        def do_work(self):
-            """Do some work"""
-            return True
-
-    x = {
-        'one': 1,
-        'two': [
-            1, 2, 3,
-            [4, 5, 6],
-        ],
-        'three': 'three',
-        'empty_class': EmptyClass(),
-        'some_class': SomeClass('some instance'),
-        'other_class': SomeOtherClass(),
-    }
-
-    module_type = ModuleType('django.html')
+    sample_empty_class = EmptyClass()
+    sample_simple_class = SimpleClass()
+    sample_complex_class = ComplexClass()
 
 
-    dump(test_func)
-    dump(test_func_param)
-    dump(x.copy)
-    dump(x, deepcopy=True)
-    dump(x, deepcopy=True)
-    x['foo'] = 'bar'
-    x['new_class'] = SomeOtherClass()
-    dump(x)
-    dump(module_type)
-    dd('hello')
+    dump(SAMPLE_CONST)
+    dump(sample_module)
+    dump(sample_bytes)
+    dump(sample_date)
+    dump(sample_datetime)
+    dump(sample_time)
+    dump(sample_int)
+    dump(sample_float)
+    dump(sample_decimal)
+    dump(sample_string)
+    dump(sample_none)
+    dump(sample_bool)
+
+    dump(sample_set)
+    dump(sample_tuple)
+    dump(sample_list)
+    dump(sample_dict)
+
+    dump(sample_complex_set)
+    dump(sample_complex_tuple)
+    dump(sample_complex_list)
+    dump(sample_complex_dict)
+
+    dump(sample_func)
+    dump(sample_func_param)
+
+    dump(sample_empty_class)
+    dump(sample_simple_class)
+    dump(sample_complex_class)
+    dump(sample_complex_class)
+
+    dump(sample_complex_list[0])
+    dump(sample_complex_tuple[0])
+    dump(sample_complex_dict['initial'])
+
+    dump(sample_complex_class._sample_private_simple_class)
+    dump(sample_complex_class.sample_public_simple_class)
+    dump(sample_complex_class.sample_public_simple_class.sample_public_dict)
+    dump(sample_complex_class.sample_public_simple_class.sample_public_dict['first'])
+
+    dump(EmptyClass)
+    dump(SimpleClass)
+    dump(ComplexClass)
+
+    dd('done')
 
     return HttpResponse("Example")
