e00aa48124353364e935ff2608fa0c293668df4e
David Barnes <barnesdavidj@gmail.com>
2022-06-08 11:57:32 -0400

    Update required packages for developpment. Add / update testing scripts. Update pylintrc to match latest version of pylint.

---
+++ runpytests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Run Pytest Tests"""
+import os
+import subprocess
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def runtests():
+    """Run tests"""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    os.environ.setdefault("PYTHONPATH", ROOT_DIR)
+    argv = ["pytest"] + sys.argv[1:]
+    subprocess.run(argv, check=True)
+
+
+if __name__ == "__main__":
+    runtests()
