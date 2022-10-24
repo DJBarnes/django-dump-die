633d0ab2019a168862f0ebdb39cb2d33a6ec0efc
David Barnes <barnesdavidj@gmail.com>
2022-10-24 19:50:58 -0400

    Bump version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, David Barnes, Brandon Rodriguez'
 author = 'David Barnes, Brandon Rodriguez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
