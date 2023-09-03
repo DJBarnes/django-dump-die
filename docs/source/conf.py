09764b007a5027cf6622f491c48e0fd6e741b9c7
David Barnes <barnesdavidj@gmail.com>
2023-09-03 12:50:31 -0400

    Bump version to 0.1.6, add support for newer versions of python and django, and remove support for older versions of python and django.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, David Barnes, Brandon Rodriguez'
 author = 'David Barnes, Brandon Rodriguez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
