0e7c5d6fb129d43ae531c62669829fdf6c896ebe
David Barnes <barnesdavidj@gmail.com>
2024-06-02 16:23:40 -0400

    Combine the runtests scripts into a single one that always uses pytest if it is available.

--- runtests.py
+++ runtests.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 """Run Django Tests"""
 import os
+import subprocess
 import sys
 from django.core.management import execute_from_command_line
+from shutil import which
 
 
-def runtests():
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def run_tests_with_pytest():
+    """Run tests"""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    os.environ.setdefault("PYTHONPATH", ROOT_DIR)
+    argv = ["pytest"] + sys.argv[1:]
+    subprocess.run(argv, check=False)
+
+
+def run_tests():
     """Run tests"""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     argv = sys.argv[:1] + ["test"] + sys.argv[1:]
@@ -13,4 +26,7 @@ def runtests():
 
 
 if __name__ == "__main__":
-    runtests()
+    if which("pytest") is not None:
+        run_tests_with_pytest()
+    else:
+        run_tests()
